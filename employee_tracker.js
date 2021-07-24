cd9fe82a89d324b95a87a34bb6417ecdec0c2972
SL <samanthl795@gmail.com>
2021-07-24 16:38:00 -0700

    Add auto increment for primary keys and add option to add a department

--- employee_tracker.js
+++ employee_tracker.js
@@ -1,36 +1,58 @@
 // Import and require inquirer
 const inquirer = require('inquirer');
 // Import and require mysql2
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 // Get the password from an envurionment variable or leave it empty if it is not specified
 const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || '';
 
-// Display the contents of a table to the console
-function showTableContents(dbConnection, tableName) {
-    var query = `SELECT * FROM ${tableName}`;
-    dbConnection.promise().query(query)
-        .then(([rows, fields]) => {
-            console.log("\n");
-            console.table(rows);
-        })
-        .catch(console.log);
+// Add a department to the database
+function addADepartment(dbConnection) {
+    return new Promise(function (resolve, reject) {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    message: 'What is the name of the department?',
+                    name: 'department_name',
+                },
+            ],
+            )
+            .then(function (response) {
+                return dbConnection.query("INSERT INTO department (name) VALUES (?)", [response.department_name]);
+            })
+            .then(([rows, fields]) => {
+                return dbConnection;
+            })
+            .then(function (db) {
+                return getNextAction(db);
+            })
+            .then((dbConnection) => dbConnection)
+            .catch(err => console.error(err));
+    }
+    )
 }
 
-//Connect to database
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        // MySQL username,
-        user: 'root',
-        // MySQL password
-        password: MYSQL_PASSWORD,
-        database: 'employee_db'
-    },
-    console.log(`Connected to the employee_db database.`)
-);
+// Display the contents of a table to the console
+function showTableContents(dbConnection, tableName) {
+    return new Promise(function (resolve, reject) {
+        var query = `SELECT * FROM ${tableName}`;
+        dbConnection.query(query)
+            .then(([rows, fields]) => {
+                console.table(rows);
+                return dbConnection;
+            })
+            .then(function (db) {
+                return getNextAction(db);
+            })
+            .then((dbConnection) => dbConnection)
+            .catch(err => console.error(err));
+    });
+}
 
-function getNextAction() {
+console.log(`Connected to the employee_db database.`)
+
+function getNextAction(db) {
     return new Promise(function (resolve, reject) {
         inquirer
             .prompt([
@@ -45,21 +67,32 @@ function getNextAction() {
             .then(function (response) {
                 switch (response.choice) {
                     case "quit":
-                        resolve();
-                        return;
+                        return db;
                     case "view all departments":
-                        showTableContents(db, "department");
-                        break;
+                        return showTableContents(db, "department");
                     case "view all roles":
-                        showTableContents(db, "role");
-                        break;
+                        return showTableContents(db, "role");
                     case "view all employees":
-                        showTableContents(db, "employee");
-                        break;
+                        return showTableContents(db, "employee");
+                    case "add a department":
+                        return addADepartment(db);
                 }
-                getNextAction().then(() => resolve())
             })
+            .then((db) => resolve(db))
+            .catch((db) => reject(db));
     });
 };
 
-getNextAction().then(() => db.end());
+//Connect to database
+mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: MYSQL_PASSWORD,
+        database: 'employee_db'
+    }
+)
+    .then((db) => getNextAction(db))
+    .then((db) => db.close());
