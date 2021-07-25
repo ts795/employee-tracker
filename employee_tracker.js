49e3c08f6ef608a65887a8e3c04eb22f68943949
SL <samanthl795@gmail.com>
2021-07-25 11:32:48 -0700

    Add import for console.table

--- employee_tracker.js
+++ employee_tracker.js
@@ -2,6 +2,8 @@
 const inquirer = require('inquirer');
 // Import and require mysql2
 const mysql = require('mysql2');
+// Import console.table
+const cTable = require('console.table');
 
 // Get the password from an envurionment variable or leave it empty if it is not specified
 const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || '';
